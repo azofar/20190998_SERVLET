933cb5582f40e893744f48d84f3e60e0363479b2
azofar <gnswhd03@gmail.com>
2022-12-06 02:39:05 +0900

    12/06 깃 정리

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <meta name="description" content="Create a stylish landing page for your business startup and get leads for the offered services with this HTML landing page template.">
+    <meta name="author" content="Inovatik">
+
+	<meta property="og:site_name" content="" />
+	<meta property="og:site" content="" />
+	<meta property="og:title" content=""/>
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:url" content="" />
+	<meta property="og:type" content="article" />
+
+    <title>LOGIN</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,600,700,700i&amp;subset=latin-ext" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/fontawesome-all.css" rel="stylesheet">
+    <link href="css/swiper.css" rel="stylesheet">
+	<link href="css/magnific-popup.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	
+</head>
+
+<body>
+
+    <form action="loginCheck.php" method="post" enctype="multipart/form-data">
+        
+        <table width = "350" height="70">
+            <tr>
+                <td><label >ID</label></td>
+                <td><input style="width:170px;height:30;" type="text" name="id"></td>
+            </tr>
+            <tr>
+                <td><label >Password</label></td> 
+                <td><input style="width:170px;height:30;" type="text" name="pw"></td>
+            </tr>
+        </table>
+        <br><br>
+        <button type="submit">로그인</button> 
+        <button type="button" onclick="location.href='member_create.html'">회원가입</button>
+        
+    </form>
+                               
+</body>
+</html>
